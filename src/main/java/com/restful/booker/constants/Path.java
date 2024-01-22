74d1bb6b1c12de543cb120e7d70996fd54349056
mahak1986 <agg.mahak1986@gmail.com>
2024-01-22 21:43:58 +0000

    SW19-restful-booker-serenity-project

---
+++ src/main/java/com/restful/booker/constants/Path.java
@@ -0,0 +1,9 @@
+package com.restful.booker.constants;
+
+public class Path {
+    /*
+     * This is base path
+     */
+    public static final String BOOKING = "/booking";
+    public static final String AUTHORIZATION = "/auth";
+}
